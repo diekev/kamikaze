056ff643d887b413ae6d4615d2942944826a4d9a
Kévin Dietrich <kevin.dietrich@mailoo.org>
2015-09-27 00:45:08 +0200

    Cleanup.

--- objects/levelset.cc
+++ objects/levelset.cc
@@ -120,62 +120,62 @@ void LevelSet::generate_mesh(openvdb::FloatGrid::ConstPtr grid)
 	Timer(__func__);
 
 	using namespace openvdb;
-    using openvdb::Index64;
+	using openvdb::Index64;
 
-    openvdb::tools::VolumeToMesh mesher(0.0);
-    mesher(*grid);
+	openvdb::tools::VolumeToMesh mesher(0.0);
+	mesher(*grid);
 
-    // Copy points and generate point normals.
-    std::vector<GLfloat> points(mesher.pointListSize() * 3);
-    std::vector<GLfloat> normals(mesher.pointListSize() * 3);
+	/* Copy points and generate point normals. */
+	std::vector<GLfloat> points(mesher.pointListSize() * 3);
+	std::vector<GLfloat> normals(mesher.pointListSize() * 3);
 
-    for (Index64 n = 0, i = 0,  N = mesher.pointListSize(); n < N; ++n) {
-        const openvdb::Vec3s& p = mesher.pointList()[n];
-        points[i++] = p[0];
-        points[i++] = p[1];
-        points[i++] = p[2];
-    }
+	for (Index64 n = 0, i = 0, N = mesher.pointListSize(); n < N; ++n) {
+		const openvdb::Vec3s& p = mesher.pointList()[n];
+		points[i++] = p[0];
+		points[i++] = p[1];
+		points[i++] = p[2];
+	}
 
-    // Copy primitives
-    openvdb::tools::PolygonPoolList& polygonPoolList = mesher.polygonPoolList();
-    Index64 numQuads = 0;
-    for (Index64 n = 0, N = mesher.polygonPoolListSize(); n < N; ++n) {
-        numQuads += polygonPoolList[n].numQuads();
-    }
+	/* Copy primitives */
+	openvdb::tools::PolygonPoolList& polygonPoolList = mesher.polygonPoolList();
+	Index64 numQuads = 0;
+	for (Index64 n = 0, N = mesher.polygonPoolListSize(); n < N; ++n) {
+		numQuads += polygonPoolList[n].numQuads();
+	}
 
-    std::vector<GLuint> indices;
-    indices.reserve(numQuads * 4);
-    openvdb::Vec3d normal, e1, e2;
+	std::vector<GLuint> indices;
+	indices.reserve(numQuads * 4);
+	openvdb::Vec3d normal, e1, e2;
 
-    for (Index64 n = 0, N = mesher.polygonPoolListSize(); n < N; ++n) {
-        const openvdb::tools::PolygonPool& polygons = polygonPoolList[n];
+	for (Index64 n = 0, N = mesher.polygonPoolListSize(); n < N; ++n) {
+		const openvdb::tools::PolygonPool& polygons = polygonPoolList[n];
 
-        for (Index64 i = 0, I = polygons.numQuads(); i < I; ++i) {
-            const openvdb::Vec4I& quad = polygons.quad(i);
-            indices.push_back(quad[0]);
-            indices.push_back(quad[1]);
-            indices.push_back(quad[2]);
-            indices.push_back(quad[3]);
+		for (Index64 i = 0, I = polygons.numQuads(); i < I; ++i) {
+			const openvdb::Vec4I& quad = polygons.quad(i);
+			indices.push_back(quad[0]);
+			indices.push_back(quad[1]);
+			indices.push_back(quad[2]);
+			indices.push_back(quad[3]);
 
-            e1 = mesher.pointList()[quad[1]];
-            e1 -= mesher.pointList()[quad[0]];
-            e2 = mesher.pointList()[quad[2]];
-            e2 -= mesher.pointList()[quad[1]];
-            normal = e1.cross(e2);
+			e1 = mesher.pointList()[quad[1]];
+			e1 -= mesher.pointList()[quad[0]];
+			e2 = mesher.pointList()[quad[2]];
+			e2 -= mesher.pointList()[quad[1]];
+			normal = e1.cross(e2);
 
-            const double length = normal.length();
-            if (length > 1.0e-7) normal *= (1.0 / length);
+			const double length = normal.length();
+			if (length > 1.0e-7) normal *= (1.0 / length);
 
-            for (int v = 0; v < 4; ++v) {
-                normals[quad[v]*3]    = static_cast<GLfloat>(-normal[0]);
-                normals[quad[v]*3+1]  = static_cast<GLfloat>(-normal[1]);
-                normals[quad[v]*3+2]  = static_cast<GLfloat>(-normal[2]);
-            }
-        }
-    }
+			for (int v = 0; v < 4; ++v) {
+				normals[quad[v] * 3]     = static_cast<GLfloat>(-normal[0]);
+				normals[quad[v] * 3 + 1] = static_cast<GLfloat>(-normal[1]);
+				normals[quad[v] * 3 + 2] = static_cast<GLfloat>(-normal[2]);
+			}
+		}
+	}
 
 	m_buffer_data->bind();
-    m_buffer_data->create_vertex_buffer(&points[0], points.size() * sizeof(GLfloat));
+	m_buffer_data->create_vertex_buffer(&points[0], points.size() * sizeof(GLfloat));
 	m_buffer_data->create_index_buffer(&indices[0], indices.size() * sizeof(GLuint));
 	m_buffer_data->attrib_pointer(m_shader["vertex"]);
 	m_buffer_data->create_color_buffer(&normals[0], normals.size() * sizeof(GLfloat));
