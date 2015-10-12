91dd6a5de1bc48654873316d19cc4fd776afaccf
Kévin Dietrich <kevin.dietrich@mailoo.org>
2015-10-12 16:09:28 +0200

    Move tree topology convertion to a dedicated functor.

--- objects/volumebase.cc
+++ objects/volumebase.cc
@@ -34,6 +34,114 @@
 #include "util/util_openvdb_process.h"
 #include "util/utils.h"
 
+struct TreeTopologyOp {
+	std::vector<glm::vec3> vertices;
+	std::vector<glm::vec3> colors;
+	std::vector<unsigned int> indices;
+
+	template <typename GridType>
+	void operator()(typename GridType::ConstPtr grid)
+	{
+		using openvdb::Index64;
+		using openvdb::math::Vec3d;
+		using openvdb::math::CoordBBox;
+
+		typedef typename GridType::TreeType TreeType;
+
+		Index64 nodeCount = grid->tree().leafCount() + grid->tree().nonLeafCount();
+	    const Index64 N = nodeCount * 8;
+
+		vertices.resize(N);
+		colors.resize(N);
+		indices.resize(N * 3);
+
+		CoordBBox bbox;
+		int idx = 0, count = 0, col_offset = 0, idx_offset = 0;
+
+		glm::vec3 node_colors[] = {
+		    glm::vec3(0.045f, 0.045f, 0.045f),         // root
+		    glm::vec3(0.0432f, 0.33f, 0.0411023f),     // first internal node level
+		    glm::vec3(0.871f, 0.394f, 0.01916f),       // intermediate internal node levels
+		    glm::vec3(0.00608299f, 0.279541f, 0.625f)  // leaf nodes
+		};
+
+		for (typename TreeType::NodeCIter iter = grid->tree().cbeginNode(); iter; ++iter) {
+	        iter.getBoundingBox(bbox);
+
+	        /* Nodes are rendered as cell-centered */
+	        Vec3d min(bbox.min().asVec3d() - Vec3d(0.5));
+	        Vec3d max(bbox.max().asVec3d() + Vec3d(0.5));
+			min = grid->indexToWorld(min);
+			max = grid->indexToWorld(max);
+
+			const glm::vec3 corners[8] = {
+			    glm::vec3(min.x(), min.y(), min.z()),
+			    glm::vec3(min.x(), min.y(), max.z()),
+			    glm::vec3(max.x(), min.y(), max.z()),
+			    glm::vec3(max.x(), min.y(), min.z()),
+			    glm::vec3(min.x(), max.y(), min.z()),
+			    glm::vec3(min.x(), max.y(), max.z()),
+			    glm::vec3(max.x(), max.y(), max.z()),
+			    glm::vec3(max.x(), max.y(), min.z()),
+			};
+
+	        vertices[count++] = corners[0];
+			vertices[count++] = corners[1];
+			vertices[count++] = corners[2];
+			vertices[count++] = corners[3];
+			vertices[count++] = corners[4];
+			vertices[count++] = corners[5];
+			vertices[count++] = corners[6];
+			vertices[count++] = corners[7];
+
+	        // edge 1
+	        indices[idx_offset++] = GLuint(idx);
+	        indices[idx_offset++] = GLuint(idx + 1);
+	        // edge 2
+	        indices[idx_offset++] = GLuint(idx + 1);
+	        indices[idx_offset++] = GLuint(idx + 2);
+	        // edge 3
+	        indices[idx_offset++] = GLuint(idx + 2);
+	        indices[idx_offset++] = GLuint(idx + 3);
+	        // edge 4
+	        indices[idx_offset++] = GLuint(idx + 3);
+	        indices[idx_offset++] = GLuint(idx);
+	        // edge 5
+	        indices[idx_offset++] = GLuint(idx + 4);
+	        indices[idx_offset++] = GLuint(idx + 5);
+	        // edge 6
+	        indices[idx_offset++] = GLuint(idx + 5);
+	        indices[idx_offset++] = GLuint(idx + 6);
+	        // edge 7
+	        indices[idx_offset++] = GLuint(idx + 6);
+	        indices[idx_offset++] = GLuint(idx + 7);
+	        // edge 8
+	        indices[idx_offset++] = GLuint(idx + 7);
+	        indices[idx_offset++] = GLuint(idx + 4);
+	        // edge 9
+	        indices[idx_offset++] = GLuint(idx);
+	        indices[idx_offset++] = GLuint(idx + 4);
+	        // edge 10
+	        indices[idx_offset++] = GLuint(idx + 1);
+	        indices[idx_offset++] = GLuint(idx + 5);
+	        // edge 11
+	        indices[idx_offset++] = GLuint(idx + 2);
+	        indices[idx_offset++] = GLuint(idx + 6);
+	        // edge 12
+	        indices[idx_offset++] = GLuint(idx + 3);
+	        indices[idx_offset++] = GLuint(idx + 7);
+
+	        idx += 8;
+
+			const int level = iter.getLevel();
+	        glm::vec3 color = node_colors[(level == 0) ? 3 : (level == 1) ? 2 : 1];
+			for (int i(0); i < 8; ++i) {
+				colors[col_offset++] = color;
+			}
+	    }
+	}
+};
+
 TreeTopology::TreeTopology(openvdb::GridBase::ConstPtr grid)
     : m_buffer_data(GPUBuffer::create())
 {
@@ -50,109 +158,16 @@ TreeTopology::TreeTopology(openvdb::GridBase::ConstPtr grid)
 	}
 	m_program.disable();
 
-	using openvdb::Index64;
-	using openvdb::math::Vec3d;
+	TreeTopologyOp op;
+	process_typed_grid(grid, get_grid_storage(*grid), op);
 
-	openvdb::FloatGrid::ConstPtr fgrid = openvdb::gridConstPtrCast<openvdb::FloatGrid>(grid);
-
-    Index64 nodeCount = fgrid->tree().leafCount() + fgrid->tree().nonLeafCount();
-    const Index64 N = nodeCount * 8;
-	m_elements = N * 3;
-
-	std::vector<glm::vec3> vertices(N);
-	std::vector<glm::vec3> colors(N);
-	std::vector<GLuint> indices(m_elements);
-
-	openvdb::CoordBBox bbox;
-	int idx = 0, count = 0, col_offset = 0, idx_offset = 0;
-
-	glm::vec3 node_colors[] = {
-	    glm::vec3(0.045f, 0.045f, 0.045f),         // root
-	    glm::vec3(0.0432f, 0.33f, 0.0411023f),     // first internal node level
-	    glm::vec3(0.871f, 0.394f, 0.01916f),       // intermediate internal node levels
-	    glm::vec3(0.00608299f, 0.279541f, 0.625f)  // leaf nodes
-	};
-
-	for (openvdb::FloatTree::NodeCIter iter = fgrid->tree().cbeginNode(); iter; ++iter) {
-        iter.getBoundingBox(bbox);
-
-        /* Nodes are rendered as cell-centered */
-        Vec3d min(bbox.min().asVec3d() - Vec3d(0.5));
-        Vec3d max(bbox.max().asVec3d() + Vec3d(0.5));
-		min = fgrid->indexToWorld(min);
-		max = fgrid->indexToWorld(max);
-
-		const glm::vec3 corners[8] = {
-		    glm::vec3(min.x(), min.y(), min.z()),
-		    glm::vec3(min.x(), min.y(), max.z()),
-		    glm::vec3(max.x(), min.y(), max.z()),
-		    glm::vec3(max.x(), min.y(), min.z()),
-		    glm::vec3(min.x(), max.y(), min.z()),
-		    glm::vec3(min.x(), max.y(), max.z()),
-		    glm::vec3(max.x(), max.y(), max.z()),
-		    glm::vec3(max.x(), max.y(), min.z()),
-		};
-
-        vertices[count++] = corners[0];
-		vertices[count++] = corners[1];
-		vertices[count++] = corners[2];
-		vertices[count++] = corners[3];
-		vertices[count++] = corners[4];
-		vertices[count++] = corners[5];
-		vertices[count++] = corners[6];
-		vertices[count++] = corners[7];
-
-        // edge 1
-        indices[idx_offset++] = GLuint(idx);
-        indices[idx_offset++] = GLuint(idx + 1);
-        // edge 2
-        indices[idx_offset++] = GLuint(idx + 1);
-        indices[idx_offset++] = GLuint(idx + 2);
-        // edge 3
-        indices[idx_offset++] = GLuint(idx + 2);
-        indices[idx_offset++] = GLuint(idx + 3);
-        // edge 4
-        indices[idx_offset++] = GLuint(idx + 3);
-        indices[idx_offset++] = GLuint(idx);
-        // edge 5
-        indices[idx_offset++] = GLuint(idx + 4);
-        indices[idx_offset++] = GLuint(idx + 5);
-        // edge 6
-        indices[idx_offset++] = GLuint(idx + 5);
-        indices[idx_offset++] = GLuint(idx + 6);
-        // edge 7
-        indices[idx_offset++] = GLuint(idx + 6);
-        indices[idx_offset++] = GLuint(idx + 7);
-        // edge 8
-        indices[idx_offset++] = GLuint(idx + 7);
-        indices[idx_offset++] = GLuint(idx + 4);
-        // edge 9
-        indices[idx_offset++] = GLuint(idx);
-        indices[idx_offset++] = GLuint(idx + 4);
-        // edge 10
-        indices[idx_offset++] = GLuint(idx + 1);
-        indices[idx_offset++] = GLuint(idx + 5);
-        // edge 11
-        indices[idx_offset++] = GLuint(idx + 2);
-        indices[idx_offset++] = GLuint(idx + 6);
-        // edge 12
-        indices[idx_offset++] = GLuint(idx + 3);
-        indices[idx_offset++] = GLuint(idx + 7);
-
-        idx += 8;
-
-		const int level = iter.getLevel();
-        glm::vec3 color = node_colors[(level == 0) ? 3 : (level == 1) ? 2 : 1];
-		for (int i(0); i < 8; ++i) {
-			colors[col_offset++] = color;
-		}
-    }
+	m_elements = op.indices.size();
 
 	m_buffer_data->bind();
-	m_buffer_data->generateVertexBuffer(&(vertices[0].x), sizeof(glm::vec3) * N);
-	m_buffer_data->generateIndexBuffer(&indices[0], sizeof(GLuint) * m_elements);
+	m_buffer_data->generateVertexBuffer(&op.vertices[0][0], sizeof(glm::vec3) * op.vertices.size());
+	m_buffer_data->generateIndexBuffer(&op.indices[0], sizeof(GLuint) * m_elements);
 	m_buffer_data->attribPointer(m_program["vertex"], 3);
-	m_buffer_data->generateNormalBuffer(&colors[0][0], sizeof(glm::vec3) * colors.size());
+	m_buffer_data->generateNormalBuffer(&op.colors[0][0], sizeof(glm::vec3) * op.colors.size());
 	m_buffer_data->attribPointer(m_program["color"], 3);
 	m_buffer_data->unbind();
 }
