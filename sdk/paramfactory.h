20f3539c2969ee822cedaad344fc78d7d0f3d6d2
Kévin Dietrich <kevin.dietrich@mailoo.org>
2016-03-17 02:26:46 +0100

    SDK: add some comments to the param factory functions.

--- sdk/paramfactory.h
+++ sdk/paramfactory.h
@@ -27,11 +27,75 @@
 class ParamCallback;
 class QString;
 
+/**
+ * @brief int_param Add a UI parameter for an integer property.
+ *
+ * @param cb   The callback used to create the parameter.
+ * @param name The UI name of the parameter.
+ * @param ptr  The pointer to the integer property.
+ * @param min  The minimum value the parameter can have.
+ * @param max  The maximum value the parameter can have.
+ * @param default_value The default value of the parameter.
+ */
 void int_param(ParamCallback *cb, const char *name, int *ptr, int min, int max, int default_value);
-void float_param(ParamCallback *cb, const char *name, float *ptr, float min, float max, float default_value);
-void enum_param(ParamCallback *cb, const char *name, int *ptr, const char *items[], int default_value);
-void string_param(ParamCallback *cb, const char *name, QString *ptr, const char *default_value);
-void bool_param(ParamCallback *cb, const char *name, bool *ptr, bool default_value);
-void xyz_param(ParamCallback *cb, const char *name, float ptr[]);
 
+/**
+ * @brief float_param Add a UI parameter for a float property.
+ *
+ * @param cb   The callback used to create the parameter.
+ * @param name The UI name of the parameter.
+ * @param ptr  The pointer to the float property.
+ * @param min  The minimum value the parameter can have.
+ * @param max  The maximum value the parameter can have.
+ * @param default_value The default value of the parameter.
+ */
+void float_param(ParamCallback *cb, const char *name, float *ptr, float min, float max, float default_value);
+
+/**
+ * @brief enum_param Add a UI parameter for an enumeration property.
+ *
+ * @param cb   The callback used to create the parameter.
+ * @param name The UI name of the parameter.
+ * @param ptr  The pointer to the enumeration property.
+ * @param items An array of names representing the possible enumeration values.
+ *              The last item needs to be set to null.
+ * @param default_value The default value of the parameter.
+ */
+void enum_param(ParamCallback *cb, const char *name, int *ptr, const char *items[], int default_value);
+
+/**
+ * @brief string_param Add a UI parameter for a string property.
+ *
+ * @param cb   The callback used to create the parameter.
+ * @param name The UI name of the parameter.
+ * @param ptr  The pointer to the string property.
+ * @param default_value Default string used as placeholder text.
+ */
+void string_param(ParamCallback *cb, const char *name, QString *ptr, const char *default_value);
+
+/**
+ * @brief bool_param Add a UI parameter for a boolean property.
+ *
+ * @param cb   The callback used to create the parameter.
+ * @param name The UI name of the parameter.
+ * @param ptr  The pointer to the boolean property.
+ * @param default_value The default value of the parameter.
+ */
+void bool_param(ParamCallback *cb, const char *name, bool *ptr, bool default_value);
+
+/**
+ * @brief bool_param Add a UI parameter for a vector property.
+ *
+ * @param cb   The callback used to create the parameter.
+ * @param name The UI name of the parameter.
+ * @param ptr  The pointer to the vector property.
+ */
+void xyz_param(ParamCallback *cb, const char *name, float ptr[3]);
+
+/**
+ * @brief param_tooltip Set the tooltip for the last added parameter.
+ *
+ * @param cb The callback used to create the parameter.
+ * @param tooltip The parameters tooltip.
+ */
 void param_tooltip(ParamCallback *cb, const char *tooltip);
