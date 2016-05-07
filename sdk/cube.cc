f73a5d2bfb0a3220099f5353e3c3c0ebd59e4018
Kévin Dietrich <kevin.dietrich@mailoo.org>
2016-05-07 04:36:54 +0200

    Cleanup.

--- sdk/cube.cc
+++ sdk/cube.cc
@@ -39,10 +39,8 @@ Cube::Cube(const glm::vec3 &min, const glm::vec3 &max)
 	m_program.enable();
 	{
 		m_program.addAttribute("vertex");
-//		m_program.addAttribute("normal");
 		m_program.addUniform("matrix");
 		m_program.addUniform("MVP");
-//		m_program.addUniform("N");
 	}
 	m_program.disable();
 
@@ -70,7 +68,6 @@ Cube::Cube(const glm::vec3 &min, const glm::vec3 &max)
 		m_vertices.push_back(vertices[i] * glm::mat3(m_inv_matrix));
 	}
 
-#if 1
 	const GLushort indices[24] = {
 	    0, 1, 1, 2,
 	    2, 3, 3, 0,
@@ -81,38 +78,12 @@ Cube::Cube(const glm::vec3 &min, const glm::vec3 &max)
 	};
 
 	m_elements = 24;
-#else
-	const GLushort indices[36] = {
-	    0, 5, 4, 5, 0, 1,
-	    3, 7, 6, 3, 6, 2,
-	    7, 4, 6, 6, 4, 5,
-	    2, 1, 3, 3, 1, 0,
-	    3, 0, 7, 7, 0, 4,
-	    6, 5, 2, 2, 5, 1
-	};
-
-	std::vector<glm::vec3> normals;
-	normals.reserve(m_vertices.size());
-
-	glm::vec3 nor, e1, e2;
-	for (int i(0); i < 24; i += 3) {
-		e1 = vertices[indices[i + 1]] - vertices[indices[i]];
-		e2 = vertices[indices[i + 2]] - vertices[indices[i + 1]];
-		nor = glm::cross(e1, e2);
-		normals.push_back(glm::normalize(nor));
-	}
-	m_elements = 36;
-#endif
 
 	m_buffer_data = ego::BufferObject::create();
 	m_buffer_data->bind();
 	m_buffer_data->generateVertexBuffer(&m_vertices[0][0], m_vertices.size() * sizeof(glm::vec3));
 	m_buffer_data->generateIndexBuffer(&indices[0], sizeof(indices));
 	m_buffer_data->attribPointer(m_program["vertex"], 3);
-#if 0
-	m_buffer_data->generateNormalBuffer(&normals[0][0], normals.size() * sizeof(glm::vec3));
-	m_buffer_data->attribPointer(m_program["normal"], 3);
-#endif
 	m_buffer_data->unbind();
 }
 
@@ -124,7 +95,6 @@ void Cube::render(ViewerContext *context, const bool /*for_outline*/)
 
 		glUniformMatrix4fv(m_program("matrix"), 1, GL_FALSE, glm::value_ptr(m_matrix));
 		glUniformMatrix4fv(m_program("MVP"), 1, GL_FALSE, glm::value_ptr(context->MVP()));
-		glUniformMatrix3fv(m_program("N"), 1, GL_FALSE, glm::value_ptr(context->normal()));
 		glDrawElements(m_draw_type, m_elements, GL_UNSIGNED_SHORT, nullptr);
 
 		m_buffer_data->unbind();
