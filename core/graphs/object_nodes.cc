c7c280abf88a3980ce19cd2fd0ebcd7ad80e6fea
Kévin Dietrich <kevin.dietrich@mailoo.org>
2016-08-04 17:34:29 +0200

    Added a preset to create a point cloud.

--- core/graphs/object_nodes.cc
+++ core/graphs/object_nodes.cc
@@ -26,6 +26,7 @@
 
 #include <kamikaze/mesh.h>
 #include <kamikaze/primitive.h>
+#include <kamikaze/prim_points.h>
 
 #include "ui/paramfactory.h"
 #include "util/utils_glm.h"
@@ -730,6 +731,59 @@ void CreateIcoSphereNode::process()
 
 /* ************************************************************************** */
 
+class CreatePointCloudNode : public Node {
+public:
+	CreatePointCloudNode();
+
+	void process() override;
+};
+
+CreatePointCloudNode::CreatePointCloudNode()
+    : Node("Point Cloud")
+{
+	addOutput("Primitive");
+
+	add_prop("Points Count", property_type::prop_int);
+	set_prop_min_max(1, 100000);
+	set_prop_default_value_int(1000);
+
+	add_prop("BBox Min", property_type::prop_vec3);
+	set_prop_default_value_vec3(glm::vec3{-1.0f, -1.0f, -1.0f});
+
+	add_prop("BBox Max", property_type::prop_vec3);
+	set_prop_default_value_vec3(glm::vec3{1.0f, 1.0f, 1.0f});
+}
+
+void CreatePointCloudNode::process()
+{
+	auto prim = m_collection->build("PrimPoints");
+	auto points = static_cast<PrimPoints *>(prim);
+
+	const auto &point_count = eval_int("Points Count");
+
+	auto point_list = points->points();
+	point_list->resize(point_count);
+
+	const auto &bbox_min = eval_vec3("BBox Min");
+	const auto &bbox_max = eval_vec3("BBox Max");
+
+	std::uniform_real_distribution<float> dist_x(bbox_min[0], bbox_max[0]);
+	std::uniform_real_distribution<float> dist_y(bbox_min[1], bbox_max[1]);
+	std::uniform_real_distribution<float> dist_z(bbox_min[2], bbox_max[2]);
+	std::mt19937 rng_x(19937);
+	std::mt19937 rng_y(19937 + 1);
+	std::mt19937 rng_z(19937 + 2);
+
+	for (size_t i = 0; i < point_count; ++i) {
+		const auto &point = glm::vec3(dist_x(rng_x), dist_y(rng_y), dist_z(rng_z));
+		(*point_list)[i] = point;
+	}
+
+	points->tagUpdate();
+}
+
+/* ************************************************************************** */
+
 void register_builtin_nodes(NodeFactory *factory)
 {
 	REGISTER_NODE("Geometry", "Box", CreateBoxNode);
@@ -740,4 +794,5 @@ void register_builtin_nodes(NodeFactory *factory)
 	REGISTER_NODE("Geometry", "Tube", CreateTubeNode);
 	REGISTER_NODE("Geometry", "IcoSphere", CreateIcoSphereNode);
 	REGISTER_NODE("Geometry", "Cone", CreateConeNode);
+	REGISTER_NODE("Geometry", "Point Cloud", CreatePointCloudNode);
 }
